87274bd000c9db9d9d71e0f4899c7fcf1c2ef3a9
jthompson <jthompson@wemicroit.com.au>
2018-11-19 00:08:03 +1100

    Added in serialising functionality

--- CSVConverter-Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSVConverter_Library
-{
-    public class Class1
-    {
-    }
-}
---
+++ CSVConverter-Library/ICSVConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeMicroIt.Utils.CSVConverter
+{
+    public interface ICSVConverter
+    {
+    }
+}
